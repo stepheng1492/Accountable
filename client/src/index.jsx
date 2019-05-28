bc8f1c4c1cfe8ecbbb26f240979d94a6c8939b36
Stephen Guillory <stepheng1492@gmail.com>
2019-05-28 17:30:30 -0500

    page rendering success

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,15 +4,31 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loggedIn: false,
+            admin: false,
+            teacher: false
+        }
     }
 
     render() {
-        return (
+        const { isLoggedIn } = this.state;
 
-            <div>
-                <h1>Fuck</h1>
-            </div>
+        if (!isLoggedIn) {
+            return (
+                <div>
+                    <form>
+                        <label>
+                            <h3>Please Sign In:</h3>
+                            <br />
+                            <input type="text" name="school" placeholder="School" />
+                            <input type="text" placeholder="Password" />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             )
+        }
     }
 }
 
