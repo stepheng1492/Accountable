a1990479c2080ab01878d7780e86c9b76b0153a8
Omar Rashid <omar.rashid2@gmail.com>
2019-05-29 12:53:27 -0500

    Post requests for comments and students complete

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('../database/index')
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -21,10 +22,46 @@ app.post('/classes', (req, res) => {
         console.log('class saved in database successfully');
         res.sendStatus(201);
     })
-    .catch(() => {
+    .catch((err) => {
         console.log('Error saving new class to database', err);
         res.sendStatus(500);
     })
-})
+});
+
+app.post('/students', (req, res) => {
+    db.models.Students.create({
+        name: req.body.name,
+        parentName: req.body.parentName,
+        phone: req.body.phone,
+        email: req.body.email,
+        comments: '[]',
+    })
+    .then(() => {
+        console.log('Student data saved in database successfully');
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.error('error saving student data in database', err);
+        res.sendStatus(500);
+    })
+});
+
+app.post('/comments', (req, res) => {
+    db.models.Comments.create({
+        studentID: req.body.studentID,
+        comment: req.body.comment,
+        date: new Date(),
+    })
+    .then(() => {
+        console.log('comment data successfully saved in database');
+        res.sendStatus(201);
+    })
+    .catch((err) => {
+        console.error('error saving comment data in database', err);
+        res.sendStatus(500);
+    })
+});
+
+
 
 app.listen(port, () => console.log(`Our app listening on port ${port}!`))
