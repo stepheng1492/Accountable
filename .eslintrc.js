697e66de8b2ddaac6ee241c2b1595c2fdea70295
Omar Rashid <omar.rashid2@gmail.com>
2019-06-01 18:15:54 -0500

    ESLINT init
     Please enter the commit message for your changes. Lines starting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
-    "extends": 
-        "airbnb-base",
-        
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
 };
-
