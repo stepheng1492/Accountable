624768717c63f3823f547b9367b852558b0c26f0
Omar Rashid <omar.rashid2@gmail.com>
2019-06-03 11:26:20 -0500

    linting and docs

--- client/src/components/Classes.jsx
+++ client/src/components/Classes.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import ClassListItem from './ClassListItem.jsx';
 
-
-// classes is list of classes by teacher
-
-const Classes = (props) => {
-    return (
-        <div>
-            <h4 display="inline-block">Your Current Classes</h4>
-            <div>
-                <h5>{props.classList.map((eachClass) => <ClassListItem eachClass={eachClass} />)}</h5>
-            </div>
-        </div>
-    );
-}
+const Classes = props => (
+  <div>
+    <h4 display="inline-block">Your Current Classes</h4>
+    <div>
+      <h5>{props.classList.map((eachClass) => <ClassListItem eachClass={eachClass} />)}</h5>
+    </div>
+  </div>
+);
 
 export default Classes;
